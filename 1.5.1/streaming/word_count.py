9e7f7ee8f27936766f16b8a256ab563148af57a5
刘桐伟 <tongwei@staff.sina.com.cn>
2015-11-03 16:46:10 +0800

    streaming word count

---
+++ 1.5.1/streaming/word_count.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+
+# create a streamingcontext
+sc = SparkContext(appName='test')
+ssc = StreamingContext(sc, 1)
+
+lines = ssc.socketTextStream('127.0.0.1', 9999)
+words = lines.flatMap(lambda line: line.split(" "))
+pairs = words.map(lambda word: (word, 1))
+wordCounts = pairs.reduceByKey(lambda x, y: x + y)
+wordCounts.pprint()
+
+
+ssc.start()
+ssc.awaitTermination()
